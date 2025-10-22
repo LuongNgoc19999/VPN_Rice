ddd9422031da0fa9a90b482e5981cb3911a0a76e
Admin <ngoc27499@gmail.com>
2025-10-22 09:44:53 +0700

    code ui item iap

--- app/src/main/java/com/chiennc/base/app/ui/fragment/iap/IapFragment.kt
+++ app/src/main/java/com/chiennc/base/app/ui/fragment/iap/IapFragment.kt
@@ -1,24 +1,41 @@
 package com.chiennc.base.app.ui.fragment.iap
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.chiennc.base.R
 import com.chiennc.base.app.ui.base.BaseFragment
-import com.chiennc.base.app.ui.fragment.result.ResultNavigation
 import com.chiennc.base.databinding.FragmentIapBinding
-import com.chiennc.base.databinding.FragmentResultBinding
+import com.chiennc.base.iap
 
-class IapFragment: BaseFragment<FragmentIapBinding, IapNavigation>() {
+class IapFragment : BaseFragment<FragmentIapBinding, IapNavigation>() {
     override fun getLayoutId(): Int = R.layout.fragment_iap
-
+    var index = 0
     override val navigation: IapNavigation = IapNavigation(this)
 
-
     override fun initView() {
-
+        binding.ivBack.setOnClickListener {
+            navigation.popBackStack()
+        }
+        binding.ervIap.withModels {
+            (0 until 3).forEach {
+                iap {
+                    id(it)
+                    textTime(
+                        when (it) {
+                            0 -> "1 month"
+                            1 -> "6 month"
+                            else -> "12 month"
+                        }
+                    )
+                    totalPrice("Total price \$${it}")
+                    price("${it}000")
+                    unit("\$/Month")
+                    isSelected(it == index)
+                    onclick { _ ->
+                        index = it
+                        binding.ervIap.requestModelBuild()
+                    }
+                }
+            }
+        }
     }
 
     override fun initObserver() {
